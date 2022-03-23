8f4c06d30c10c0de2f8ce35f508c16068b0a590d
lykmast <lykmast@gmail.com>
2022-03-23 14:09:44 +0200

    Add isInfinite co-predicate and a proof using it.

--- src/SizedTypes/ListProofs.hs
+++ src/SizedTypes/ListProofs.hs
@@ -5,7 +5,7 @@
 {-@ LIQUID "--higherorder" @-}
 module SizedTypes.ListProofs where
 
-import Prelude hiding (repeat, zipWith, map, hd, tl)
+import Prelude hiding (repeat, zipWith, map, hd, tl, isInfinite,not)
 import Language.Haskell.Liquid.ProofCombinators
 import SizedTypes.Size
 
@@ -27,7 +27,9 @@ tl (Cons _ xs) = xs
 {-@ reflect repeat @-}
 repeat x = Cons x (repeat x)
 
+{-@ lazy map @-}
 {-@ reflect map @-}
+{-@ map :: _ -> xs:_ -> {v:_|emp xs <=> emp v} @-}
 map :: (a -> b) -> List a -> List b
 map _ Nil = Nil
 map f xs  = Cons (f (hd xs)) $ map f (tl xs)
@@ -136,4 +138,42 @@ theoremMapAppend i f m n = bisim i (map f (append m n))
       === hd (f (hd m) `Cons` map f (tl m)) `Cons`
           append (tl (f (hd m) `Cons` map f (tl m))) (map f n)
       === f (hd m) `Cons` append (map f (tl m)) (map f n)
+
+{-@ reflect isInfinite @-}
+isInfinite Nil = False
+isInfinite xs  = isInfinite (tl xs)
+
+
+
+{-@ assume isInfiniteS :: i:Size
+                       -> xs:ListNE _
+                       -> ({j:Size| j<i} -> {isInfinite (tl xs)})
+                       -> {isInfinite xs}
+@-}
+isInfiniteS :: Size
+            -> List a
+            -> (Size -> Proof)
+            -> Proof
+isInfiniteS i xs p = ()
+
+{-@ reflect not @-}
+not True  = False
+not False = True
+
+{-@ reflect isFinite @-}
+isFinite = not . isInfinite
+
+{-@ lemmaMapInfinite :: i:Size
+                     -> f:_ -> {xs:_|isInfinite xs}
+                     -> {isInfinite (map f xs)}
+@-}
+lemmaMapInfinite :: Size -> (a -> b) -> List a -> Proof
+lemmaMapInfinite i f xs@Nil = isInfinite xs === False *** QED
+lemmaMapInfinite i f xs = isInfiniteS i (map f xs) $ \j
+  ->  tl (map f xs)
+  === tl (Cons (f (hd xs)) (map f (tl xs)))
+  === map f (tl xs)
+    ? (isInfinite xs === isInfinite (tl xs) *** QED)
+    ? lemmaMapInfinite j f (tl xs)
+  *** QED
 
